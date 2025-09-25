19908673b51611b5328c79b4a2095583cb044967
otomasyon <140910406+aziz2055@users.noreply.github.com>
2025-09-25 21:52:59 +0300

    Create chatbot-12.js

---
+++ chatbot-12.js
@@ -0,0 +1,22 @@
+<script>
+window.ChatWidgetConfig = {
+  webhook: {
+    url: 'https://n8n.n8naziz.com/webhook/ec6ff37a-76be-415e-880b-da4df99fc075/chat',
+    route: 'general'
+  },
+  branding: {
+    logo: '<your company logo URL>',
+    name: 'nocodecreative.io', // Your company name
+    welcomeText: 'Hi 👋, how can we help?', // Welcome message
+    responseTextTime: 'We typically respond right away' // Response time
+  },
+  style: {
+    primaryColor: '#854fff', // Primary color
+    secondaryColor: '#6b3fd4', // Secondary color
+    position: 'right', // Position of the widget (left or right)
+    backgroundColor: '#ffffff', // Background color of the chat widget
+    fontColor: '#333333' // Text color for messages and interface
+  }
+};
+</script>
+<script src="https://cdn.jsdelivr.net/gh/aziz2055/my-chat-widget@main/wp-12.js"></script>
